bfbac94b74ccf403e6c4ed24c58082f71d74f261
DanielMBustamante <danieljohn.bustamante@smc.pshs.edu.ph>
2026-03-04 05:29:11 +0800

    Add BUSTAMANTE_Q3_FA07.py file

---
+++ BUSTAMANTE_Q3_FA07.py
@@ -0,0 +1,22 @@
+students = []
+
+for i in range(3):
+    dict = {
+    "name":"",
+    "age":0,
+    "grade":0
+}
+    name = str(input("Enter name: "))
+    dict["name"] = name
+    age = int(input("Enter age: "))
+    dict["age"] = age
+    grade = int(input("Enter grade: "))
+    dict["grade"] = grade
+    students.append(dict)
+    print("")
+
+print("Class Directory:")
+counter = 0
+for s in students:
+    print(f"{students[counter]["name"]} | Age: {students[counter]["age"]} | Grade: {students[counter]["grade"]}")
+    counter += 1
